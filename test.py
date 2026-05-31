4876778a998d6998619ba5f93425ea75af7c2fe7
Edgar <melqumyanedgar04@gmail.com>
2026-05-31 14:57:42 +0400

    version one

---
+++ test.py
@@ -0,0 +1,16 @@
+# def name():
+#     x = int(input("x: "))
+#     y = int(input("y: "))
+#     z = input("z: ")
+#     if z == "+":
+#         print(x + y)
+#     elif z == "-":
+#         print(x - y)
+#     elif z == "*":
+#         print(x * y)
+#     elif z == "/":
+#         print(x / y)
+
+# name()
+import random
+random_number = random.randint(1, 100)
